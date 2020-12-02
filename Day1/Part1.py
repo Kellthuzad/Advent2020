116babbeccd96aba42ced73cceee46e66c17d234
Kellthuzad <kellen.f.swain@gmail.com>
2020-12-02 00:42:55 +0000

    stuff

--- Day1/Part1.py
+++ Day1/Part1.py
@@ -1,1 +1,21 @@
+import urllib.request as Request
 
+
+def main():
+    filehandle = open("./Day1/input.txt")
+    lines = []
+    numbers = []
+    for line in filehandle:
+        lines.append(line)
+        numbers.append(int(line))
+    print(find2020sum(numbers))
+
+def find2020sum(numbers):
+    for x in numbers:
+       for y in numbers:
+           for z in numbers:
+                if (x + y + z == 2020):
+                    return x*y*z
+                    
+
+main()
